49dbb05ccbb8ffdfb3117f21de015ad3778f366e
Arban Hossain <arbanhossain@gmail.com>
2023-02-26 12:47:26 +0600

    util for setting render layers

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -7,4 +7,10 @@ public static class Utils
     public static Vector3 GetRandomSpawnPoint() {
         return new Vector3(Random.Range(-20, 20), 4, Random.Range(-20, 20));
     }
+
+    public static void SetRenderLayerInChildren(Transform _Transform, int LayerNumber) {
+        foreach(Transform T in _Transform.GetComponentInChildren<Transform>(true)) {
+            T.gameObject.layer = LayerNumber;
+        }
+    }
 }
