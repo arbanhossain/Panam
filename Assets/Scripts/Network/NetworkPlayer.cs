76b0b89ba83fe1abdb737e0b19e9ffd7cd40f6be
Arban Hossain <arbanhossain@gmail.com>
2023-02-26 12:48:07 +0600

    render only remote, fix camera changing bug, disable audio listener

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -8,6 +8,8 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
 {
     public static NetworkPlayer Local { get; set; }
 
+    public Transform PlayerModel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,19 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
     public override void Spawned() {
         if (Object.HasInputAuthority) {
             Local = this;
+
+            Utils.SetRenderLayerInChildren(PlayerModel, LayerMask.NameToLayer("LocalPlayerModel"));
+
+            Camera.main.gameObject.SetActive(false);
+
             Debug.Log($"Local Player Spawned");
         } else {
+            Camera LocalCam = GetComponentInChildren<Camera>();
+            LocalCam.enabled = false;
+
+            AudioListener LocalAudioListener = GetComponentInChildren<AudioListener>();
+            LocalAudioListener.enabled = false;
+
             Debug.Log($"Remote Player Spawned");
         }
     }
